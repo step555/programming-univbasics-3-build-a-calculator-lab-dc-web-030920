637ac6063a179fd6330e6b4e075e71a2a73f068a
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-16 15:48:48 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,15 +7,15 @@ difference = num1 - num2
 end
 
 def division(num1, num2)
-
+quotient = num1 / num2
 end
 
 def multiplication(num1, num2)
-
+product = num1 * num2
 end
 
 def modulo(num1, num2)
-
+modulus = num1 % num2
 end
 
 def square_root(num)
